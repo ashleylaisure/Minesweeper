c486129d873ef1efe6406d1655028b2ef293cd53
Ashley Laisure <ashley.laisure@gmail.com>
2024-12-11 11:55:10 -0500

    Intial design and flag counter

--- js/app.js
+++ js/app.js
@@ -18,6 +18,37 @@ for (let i = 0; i < rows; i++) {
         // add each square to grid
         gameGrid.appendChild(gridSquare);
 
-        gridSquare.textContent = gridSquare.id
+        // gridSquare.textContent = gridSquare.id
     }
 }
+// Bombs & Flags
+
+let bombs = 10;
+
+const flagCountdown = document.querySelector('.bomb-counter')
+console.dir(flagCountdown)
+
+flagCountdown.textContent = bombs;
+
+// rightClick call back function
+const rightClick = (event) => {
+    // prevent the default menu from appearing
+    event.preventDefault();
+    if (event.target.textContent === '') {
+        // change textContent to flag
+        event.target.textContent = '🚩'
+        // decrease flag counter
+        bombs = bombs - 1;
+        flagCountdown.textContent = bombs;
+    } else if (event.target.textContent === '🚩'){
+        event.target.textContent = ''
+        bombs++;
+        flagCountdown.textContent = bombs;
+    }
+
+    
+};
+
+
+// add right click event listener for the gameGrid with callback function
+gameGrid.addEventListener("contextmenu", rightClick);
